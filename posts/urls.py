e1335503bb9320759f606ee50d20481c6ce79465
rr2x <79620740+rr2x@users.noreply.github.com>
2021-06-10 16:46:01 +0800

    ViewSet and Routers

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,21 @@
+from django.db.models import base
 from django.urls import path
-from .views import PostList, PostDetail
+from rest_framework import urlpatterns
+from rest_framework.routers import SimpleRouter
+#from .views import PostList, PostDetail, UserList, UserDetail
+from .views import UserViewSet, PostViewSet
 
-urlpatterns = [
-    path('<int:pk>', PostDetail.as_view()),
-    path('', PostList.as_view()),
-]
+# urlpatterns = [
+#     path('users/', UserList.as_view()),
+#     path('users/<int:pk>/', UserDetail.as_view()),
+#     path('', PostList.as_view()),
+#     path('<int:pk>', PostDetail.as_view()),
+# ]
+
+
+# using routers
+router = SimpleRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('', PostViewSet, basename='posts')
+
+urlpatterns = router.urls
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,24 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics
+from rest_framework import viewsets
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
+
+
+# --- using viewsets ---
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+# --- using viewsets ---
 
 
 class PostList(generics.ListCreateAPIView):
@@ -13,3 +30,13 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
